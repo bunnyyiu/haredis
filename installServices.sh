d41bdbe9a9191e3f9a7f87ffe944fe6dc326fd2f
Bunny Yiu <bunnyyiu@users.noreply.github.com>
2015-08-15 14:36:25 +0000

    fix minor issue on service start

--- installServices.sh
+++ installServices.sh
@@ -3,6 +3,7 @@
 chown -R redis:redis /var/run/redis
 chown -R redis:redis /var/log/redis
 chown -R redis:redis /var/lib/redis
+chown -R redis:redis /etc/redis
 
 chown redis:redis /etc/twemproxy.cfg
 
@@ -11,5 +12,7 @@ do
   cp $f /etc/init.d/
   name=${f##*/}
   update-rc.d $name defaults
+  systemctl daemon-reload
+  systemctl enable $name
+  service $name start
 done
-systemctl daemon-reload
